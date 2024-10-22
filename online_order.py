c83aff40b952afa85fe0f53bd6fde90f53a6c58b
HemachandarK <138129846+HemachandarK@users.noreply.github.com>
2024-10-23 02:26:17 +0530

    Update online_order.py

--- online_order.py
+++ online_order.py
@@ -19,14 +19,14 @@ from httplib2 import Http
 
 def online_order():
     client_secret = st.secrets["client_secret"]
-
+    client_secret_dict = json.loads(client_secret)
     SCOPES = ['https://www.googleapis.com/auth/gmail.modify',
             'https://www.googleapis.com/auth/userinfo.email',
             'https://www.googleapis.com/auth/userinfo.profile']
     store = file.Storage('storage.json')
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets(client_secret, SCOPES)
+        flow = client.flow_from_clientsecrets(client_secret_dict, SCOPES)
         creds = tools.run_flow(flow, store)
     GMAIL = discovery.build('gmail', 'v1', http=creds.authorize(Http()))
     user_info_service = discovery.build('oauth2', 'v2', http=creds.authorize(Http()))
